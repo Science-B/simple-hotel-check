98202ae1a16c28f1c4580c3e9a180759b88d4469
Science-B <dmitriytwd@gmail.com>
2023-01-17 15:12:03 +0300

    install redux

--- src/app/components/header/header.jsx
+++ src/app/components/header/header.jsx
@@ -1,12 +1,20 @@
+import { useNavigate } from "react-router-dom";
+
 import s from "./header.module.scss";
 
 import logOutIcon from "../../icons/logOut.svg";
 
 export function Header() {
+  let navigate = useNavigate();
+
+  function handleExit() {
+    navigate("/");
+  }
+
   return (
     <div className={s.header}>
       <div className={s.name}>Simple Hotel Check</div>
-      <div className={s.logOutGroup}>
+      <div className={s.logOutGroup} onClick={handleExit}>
         <span>Выйти</span>
         <img className={s.logOutIcon} src={logOutIcon} alt="logOutIcon" />
       </div>
